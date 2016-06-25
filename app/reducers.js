9258e6e947cd7f0dc1a87aa7d852402c0b92ddbd
Codesign <cs@csdev.io>
2016-06-25 18:08:49 +0100

    Added a basic auth reducer

--- app/reducers.js
+++ app/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
+import { reducer as auth } from './redux/auth';
 
 // Combine reducers with routeReducer which keeps track of
 // router state
 const rootReducer = combineReducers({
-  routing
+  routing,
+  auth
 });
 
 export default rootReducer;
---
+++ app/redux/auth.js
@@ -0,0 +1,10 @@
+const initialState = {
+  token: null,
+  currentUser: null,
+  socket: null,
+  error: null
+};
+
+export function reducer(state = initialState, action = {}) {
+  return state;
+}
