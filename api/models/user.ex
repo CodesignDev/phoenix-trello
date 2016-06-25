f7659f3a0effcb55251c5a40965fa8e492af521b
Codesign <cs@csdev.io>
2016-06-25 19:48:06 +0100

    Added password encryption via comeonin

--- api/models/user.ex
+++ api/models/user.ex
@@ -22,5 +22,15 @@ defmodule PhoenixTrello.User do
     |> validate_confirmation(:password, message: "Password does not match")
     |> unique_constraint(:username, message: "Username already in use")
     |> unique_constraint(:email, message: "Email already in use")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
   end
 end
