5ef54912e61b5a22e511a018f3b7dd6333b2acb1
Codesign <cs@csdev.io>
2016-07-02 20:52:27 +0100

    Ohhh, so that's why the JWT token wasn't working. *headdesk*

--- api/router.ex
+++ api/router.ex
@@ -3,6 +3,9 @@ defmodule PhoenixTrello.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api", PhoenixTrello do
